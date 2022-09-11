2681392b54f34dfb8b1e9bd36bedb939abc37d86
Manoj Bellamkonda <manoj523arjun@users.noreply.github.com>
2022-09-11 20:58:40 +0530

    Add files via upload
    Initial setup

---
+++ src/components/DragListItem.js
@@ -0,0 +1,67 @@
+import React, { useCallback, useState, useEffect, useRef } from 'react';
+
+const DragListItem = ({
+  listItem,
+  labelTextPropName,
+  labelIdPropName,
+  onRowClickHandler,
+  selectedList,
+  onMouseDownHandler,
+  onMouseUpHandler,
+  onDragHandler
+}) => {
+  const [isSelected, setSelected] = useState();
+
+  const onRowClick = useCallback(() => {
+    setSelected(!isSelected);
+    // if (selectedList[listItem[labelIdPropName]]) {
+    //     delete selectedList[listItem[labelIdPropName]];
+    // } else {
+    //     selectedList[listItem[labelIdPropName]] = listItem;
+    // }
+    onRowClickHandler(!isSelected, listItem, listItem[labelIdPropName]);
+  }, [isSelected]);
+
+  const onDrag = (e) => {
+    if (onDragHandler) {
+      onDragHandler(e, selectedList);
+    }
+  };
+
+  const onMouseUp = (e) => {
+    if (onMouseUpHandler) {
+      onMouseUpHandler(e, selectedList);
+    }
+    // setTimeout(() => {
+    document.removeEventListener('mousemove', onDrag);
+    document.removeEventListener('mouseup', onMouseUp);
+    // }, 100);
+  };
+
+  const onMouseDown = useCallback(
+    (e) => {
+      e.preventDefault();
+      if (e.nativeEvent.which === 1 || e.button === 0) {
+        if (onMouseDownHandler) {
+          onMouseDownHandler(e);
+        }
+        document.addEventListener('mousemove', onDrag);
+        document.addEventListener('mouseup', onMouseUp);
+      }
+    },
+    [onMouseDownHandler]
+  );
+
+  return (
+    <div
+      id={listItem[labelIdPropName]}
+      className={`drag-list-item${isSelected ? ' drag-list-item-selected' : ''}`}
+      onClick={onRowClick}
+      onMouseDown={onMouseDown}
+    >
+      {listItem[labelTextPropName]}
+    </div>
+  );
+};
+
+export default DragListItem;
